f9947872cdd7eabe82a9fd0465b6bc189c0ab3d3
周文耿 <412582902@qq.com>
2019-04-01 23:57:20 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,11 +11,16 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
+  
   def update
     @group = Group.find(params[:id])
     @group.update(group_params)
