467740ebab975a94cc68f4d683e3faee9aed771f
周文耿 <412582902@qq.com>
2019-04-01 23:26:16 +0800

    implement gtoups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,9 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+  def edit
+    @group = Group.find(params[:id])
+  end
   def new
     @group = Group.new
   end
